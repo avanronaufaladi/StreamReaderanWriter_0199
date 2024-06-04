30f5bc5c5d420ef22670026cb3d7aecd0850d03d
avanronaufaladi <avaroadi@gmail.com>
2024-06-04 10:31:29 +0700

    memasukkan nilai kedalam baris

--- StreamReaderanWriter_0199.cpp
+++ StreamReaderanWriter_0199.cpp
@@ -22,4 +22,7 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukkan karakter q
 		if (baris == "q")break;
-		
+		//menulis dan memasukkan nilai dari 'baris' kedalam file
+		outfile << baris << endl;
+	}
+	
